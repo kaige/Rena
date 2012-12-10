667495540ef80d74d546cc58163cb7e6d93c098e
Maowen <maowen.zhang@hotmail.com>
2012-12-10 10:16:30 +0800

    add some changes in toolbar

--- Source/public/js/testToolbar.js
+++ Source/public/js/testToolbar.js
@@ -1,12 +1,39 @@
 
-// Decorate the third toolbar.	
-    var t3 = new goog.ui.Toolbar();
-    t3.decorate(goog.dom.getElement('t3'));
 
-    // Have the alignment buttons be controlled by a selection model.
-    var selectionModel = new goog.ui.SelectionModel();
-    selectionModel.setSelectionHandler(function(button, select) {
-      if (button) {
-        button.setChecked(select);
-      }
-    });
+var g_toolbarButtons = [];
+
+var skToolbarBtn = function (name, tooltip, classname) {
+    this.name = name;
+    this.tooltip = tooltip;
+	this.classname = classname;
+}
+
+var addToolbarButton = function (name, tooltip, classname) {
+    var btn = new skToolbarBtn(name, tooltip, classname);
+	g_toolbarButtons.push(btn);
+}
+
+var initToolbar = function () {
+
+	var toolbar = new goog.ui.Toolbar();
+
+    addToolbarButton("Point", "point", "icon toolbar-point");
+	addToolbarButton("Line", "line", "icon toolbar-line");
+	addToolbarButton("Circle", "circle", "icon toolbar-circle");
+	addToolbarButton("Rectangle", "rectangle", "icon toolbar-rectangle");
+
+    goog.array.forEach(g_toolbarButtons, function (btn, index, array) {
+		var tbtn = new goog.ui.ToolbarToggleButton(goog.dom.createDom('div',
+        btn.classname));
+		
+		tbtn.setTooltip(btn.tooltip);
+        toolbar.addChild(tbtn, true);
+    });	
+	
+	// toolbar.addChild(new goog.ui.ToolbarButton('test button'), true);
+	toolbar.render(goog.dom.getElement('tool_bar_group1_div'));
+	toolbar.render(goog.dom.getElement('tool_bar_group2_div'));
+	toolbar.render(goog.dom.getElement('tool_bar_group3_div'));
+}
+
+initToolbar();
