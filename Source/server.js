7ca04027814dc457b5d989cdf5bad58b32f23b43
kaizhang <kzhang80@gmail.com>
2013-12-18 10:31:08 +0800

    modify css style so the drawing area has a gap with tool bar

--- Source/server.js
+++ Source/server.js
@@ -2,4 +2,4 @@ var http = require('http');
 var ecstatic = require('ecstatic')(__dirname + '/public');
 var server = http.createServer(ecstatic);
 server.listen(8080);
-console.log("started server at default port 80, try 'localhost' in browser");
+console.log("started server at default port 8080, try 'localhost' in browser");
